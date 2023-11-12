e78aa09c43d25c9ddbfa4f8df29babd74101e4e8
Steven Colbert <tannerjcolbert@gmail.com>
2023-11-12 14:52:38 -0800

    TEC-3 and TEC-4: React Elements Functionality and CSS

--- Login/src/pages/Login.js
+++ Login/src/pages/Login.js
@@ -1,6 +1,20 @@
 import { useState } from "react";
 
+import {
+  TextField,
+  InputAdornment,
+  Icon,
+  IconButton,
+  Checkbox,
+  FormControlLabel,
+  Button,
+} from "@mui/material";
+
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPasswordClick = () => {
+    setShowPassword(!showPassword);
+  };
   
   return (
     <div className="relative w-full flex flex-row items-center justify-between text-center text-5xl text-additional-white font-body-small-regular">
@@ -33,10 +47,107 @@ const Login = () => {
                 </div>
               </div>
             </div>
-          </div>
+          <div className="w-[404px] flex flex-col items-start justify-start gap-[16px]">
+            <TextField
+              className="[border:none] bg-[transparent] self-stretch"
+              color="error"
+              name="Email"
+              label="Email"
+              size="large"
+              required={true}
+              variant="outlined"
+              type="email"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icon>mail_sharp</Icon>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              className="[border:none] bg-[transparent] self-stretch"
+              color="error"
+              name="Password"
+              label="Password"
+              size="large"
+              required={true}
+              variant="outlined"
+              type={showPassword ? "text" : "password"}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icon>lock_sharp</Icon>
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleShowPasswordClick}
+                      aria-label="toggle password visibility"
+                    >
+                      <Icon>
+                        {showPassword ? "visibility_off" : "visibility"}
+                      </Icon>
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
         </div>
     </div>
-     
+    <div className="self-stretch overflow-hidden flex flex-col items-center justify-start gap-[32px] text-center text-sm">
+          <div className="self-stretch overflow-hidden flex flex-col items-center justify-center text-left">
+            <div className="w-[403px] h-6 overflow-hidden shrink-0 flex flex-row items-center justify-start gap-[145px]">
+              <div className="flex flex-row items-center justify-start gap-[12px]">
+                <FormControlLabel
+                  className="relative"
+                  label=""
+                  control={<Checkbox color="error" defaultChecked />}
+                />
+                <div className="relative tracking-[0.2px] leading-[160%] font-medium">
+                  Remember me
+                </div>
+              </div>
+              <a className="[text-decoration:none] relative leading-[160%] font-bold text-primary-600-base text-right">
+                Forgot Password?
+              </a>
+            </div>
+          </div>
+          <Button
+            className="self-stretch"
+            color="error"
+            name="Sign In"
+            size="large"
+            variant="contained"
+          >
+            Sign In
+          </Button>
+          <div className="self-stretch flex flex-row items-center justify-start gap-[16px] text-xs text-greyscale-500">
+            <div className="flex-1 relative box-border h-px border-t-[1px] border-solid border-greyscale-200" />
+            <div className="relative leading-[160%]">Or sign in with</div>
+            <div className="flex-1 relative box-border h-px border-t-[1px] border-solid border-greyscale-200" />
+          </div>
+          <div className="self-stretch overflow-hidden flex flex-col items-center justify-center">
+            <div className="w-[403px] flex flex-row items-start justify-start gap-[16px]">
+              <Button className="flex-1" color="error" variant="contained">
+                Google
+              </Button>
+              <Button className="flex-1" color="error" variant="contained">
+                Facebook
+              </Button>
+            </div>
+          </div>
+          <a className="[text-decoration:none] relative leading-[160%] text-[inherit]">
+            <span>
+              <span>Don’t have an account?</span>
+              <span className="tracking-[0.2px] font-medium font-body-small-regular">{` `}</span>
+            </span>
+            <b className="text-primary-600-base">Sign Up</b>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
 
