9fe463734430dbe3f77ed9a4c605f9352c3d710f
KevinDeol <54293413+KevinDeol@users.noreply.github.com>
2023-11-12 20:28:47 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import './Bay Develops Top Nav Bar/src/global.css';
+import './Bay Develops Top Nav Bar/tailwind.config.js';
+import './Bay Develops Side Nav Bar/src/global.css';
+import './Bay Develops Side Nav Bar/tailwind.config.js';
+import ContactUs from './ContactUs.js';
+import TopNavbar from './Bay Develops Top Nav Bar/src/components/TopNavbar.js';
+import SideNavbar from './Bay Develops Side Nav Bar/src/pages/NavigationDark.js';
+
+function App() {
+  return (
+    <div>
+      <ContactUs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Bay Develops Top Nav Bar/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#fff",
+        gray: "#141416",
+      },
+      spacing: {},
+      fontFamily: {
+        abeezee: "ABeeZee",
+      },
+    },
+    fontSize: {
+      base: "16px",
+      inherit: "inherit",
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
