1a9f9778c54cff73cf934113ac66155f1e8e4626
Chandi <schandi@csus.edu>
2024-02-11 16:54:24 -0800

    TEC 121

---
+++ src/components/ems.tsx
@@ -0,0 +1,50 @@
+import BarChartBox from "./emsFiles/barChartBox/BarChartBox.tsx";
+import BigChartBox from "./emsFiles/bigChartBox/BigChartBox.tsx";
+import ChartBox from "./emsFiles/chartBox/ChartBox.tsx";
+import PieChartBox from "./emsFiles/pieCartBox/PieChartBox.tsx";
+import TopBox from "./emsFiles/topBox/TopBox.tsx";
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "./emsFiles/data.ts";
+import "./ems.scss";
+import React from 'react';
+export const EMS = (props) => {
+  return (
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box7">
+        <BigChartBox />
+      </div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
+    </div>
+  );
+};
+
+export default EMS;
