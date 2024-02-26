bd2612303568e1f3ab0b77bdbc2d4d1bf1f4fb09
Steven Colbert <tannerjcolbert@gmail.com>
2024-02-25 17:52:37 -0800

    TEC-127 Emp Dash To Do List

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ import { SideNavDark } from "./components/sideNavDark";
 import {ServicesPage} from "./components/ServicesPage";
 import { Resetpassword } from "./components/resetpassword";
 import {EMS} from "./components/ems.tsx";
-//import {App} from "./components/UserDashboard.js";
+import {ProfilePage} from "./components/ProfilePage";
+import {UserDash} from "./components/UserDashboard";
 import SmoothScroll from "smooth-scroll";
 import "./index.css";
 
@@ -26,13 +27,18 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
 const App = () => {
   return (
     <Router>
+      <TopNav />
+      <SideNavDark />
       <Routes>
-        <Route path="/about" element={<ContactUs />} />
-        <Route path="/services" element={<EMS />} />
+      <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<ServicesPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/reset-password" element={<Resetpassword />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/user-dashboard" element={<UserDash />} />
         {/* Add other routes as needed */}
       </Routes>
     </Router>
