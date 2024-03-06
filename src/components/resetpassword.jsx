36ff2e4d47b2d3d20bc9d4cca83b00ad459794c7
Steven Colbert <tannerjcolbert@gmail.com>
2024-03-05 20:06:48 -0800

    Verify Email and Reset Password Page Restructure

--- src/components/resetpassword.jsx
+++ src/components/resetpassword.jsx
@@ -38,6 +38,17 @@ export const ResetPassword = () => {
         <div className="max-w-fit w-full">
           <h2 className="text-3xl font-bold mb-2">Set New Password</h2>
           <form className="mt-8 space-y-6" onSubmit={handleSetNewPassword}>
+          <div>
+              <label htmlFor="reset-code" className="sr-only">Reset Code</label>
+              <input 
+                id="reset-code" 
+                name="reset-code" 
+                type="password" 
+                required 
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 rounded-b-md" 
+                placeholder="Enter Code"
+              />
+            </div>
             <div>
               <label htmlFor="new-password" className="sr-only">New Password</label>
               <input 
