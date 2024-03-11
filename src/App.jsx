c2e906997c5a9a40378318f609ff90036582387f
Chandi <schandi@csus.edu>
2024-03-10 22:18:20 -0700

    working main

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
+
+//import { Chat } from "./components/Chat";
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-//import { About } from "./components/About";
-import { About } from "./components/AppDevServicePage.js";
-
-import { ContactUs } from "./components/ContactUs";
+import { About } from "./components/About";
+//import { About } from "./components/AppDevServicePage.js";
+import { ContactUs } from "./components/ContactUs.jsx";
 import { Landing } from "./components/landing";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 import { TopNav } from "./components/TopNavbar";
 import { SideNavDark } from "./components/sideNavDark";
-import {ServicesPage} from "./components/ServicesPage";
-import { Resetpassword } from "./components/resetpassword";
-import {EMS} from "./components/ems.tsx";
-import {App} from "./components/UserDashboard.js";
+import { ProfilePage } from "./components/ProfilePage"
+import { UserDash } from "./components/UserDashboard"
+import { ServicesPage } from "./components/ServicesPage";
+import { VerifyEmailPage } from "./components/VerifyEmailPage";
+import { ResetPassword } from "./components/resetpassword";
+import { EMS } from "./components/ems.tsx";
+import { CreditCardForm } from "./components/paymentPage"; 
+//import {App} from "./components/UserDashboard.js";
 import SmoothScroll from "smooth-scroll";
 import "./index.css";
-import { Chat } from "./components/Chat";
-
-
+import { UIUXDevServicePage } from "./components/UIUXDevServicePage"
+import { WebDevServicePage } from "./components/WebDevServicePage";
+import { AppDevServicePage } from "./components/AppDevServicePage"
+import { SoftwareDevServicePage } from "./components/SoftwareDevServicePage";
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
@@ -40,11 +47,11 @@ const App = () => {
         <Route path="/software-services" element={<SoftwareDevServicePage />} />  
         
         
-        <Route path="/appDev-services" element ={<AppDevServicePage />} />
+        <Route path="/appDev-services" element ={<UIUXDevServicePage />} />
       
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<ContactUs />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path ="/paymentPage" element={<CreditCardForm/>}/>
         <Route path="/verify-email" element={<VerifyEmailPage />} />
