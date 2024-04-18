792c05ccb3a58a1135abe030b9f9412498bae7fa
pateld1p <111032586+pateld1p@users.noreply.github.com>
2024-04-17 21:42:32 -0700

    fixed verify email page issue

--- src/components/VerifyEmailPage.js
+++ src/components/VerifyEmailPage.js
@@ -22,11 +22,6 @@ export const VerifyEmailPage = () => {
   return (
     <div className="w-full relative bg-additional-white overflow-hidden flex flex-row items-start justify-start pt-0 px-0 pb-[82px] box-border gap-[41px] tracking-[normal] mq700:gap-[41px] mq925:flex-wrap">
       <div className="h-[346px] w-[437px] flex flex-col items-start justify-end py-0 px-0 box-border min-w-[437px] max-w-full mq700:min-w-full mq925:flex-1">
-        <img
-          className="w-[500px] h-[500px] relative object-cover shrink-0 max-w-[114%] mq925:self-stretch mq925:w-auto"
-          alt=""
-          src="./img/bayDevelopsLogo(black).png"
-        />
       </div>
       <div className="w-[484px] flex flex-col items-start justify-start pt-[82px] px-0 pb-0 box-border min-w-[484px] max-w-full mq700:min-w-full mq450:pt-[34px] mq450:box-border mq925:flex-1 mq925:pt-[53px] mq925:box-border">
         <form onSubmit = {handleSendCode} className="m-0 self-stretch rounded-2xl bg-additional-white flex flex-col items-center justify-center p-10 gap-[32px] mq700:gap-[32px] mq925:pt-[26px] mq925:pb-[26px] mq925:box-border">
