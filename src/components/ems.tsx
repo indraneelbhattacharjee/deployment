a8bc2486c942f5ee80fc2f5264ca0b0c900a8355
Indraneel Bhattacharjee <indraneelbhattacharjee24@gmail.com>
2024-04-30 00:17:03 -0700

    commiting npm changes and adding wrapper scripts

--- src/components/ems.tsx
+++ src/components/ems.tsx
@@ -1,17 +1,12 @@
-//import BarChartBox from "./emsFiles/barChartBox/BarChartBox.tsx";
-import BigChartBox from "./emsFiles/bigChartBox/BigChartBox.tsx";
-import ChartBox from "./emsFiles/chartBox/ChartBox.tsx";
-//import PieChartBox from "./emsFiles/pieCartBox/PieChartBox.tsx";
-import TopBox from "./emsFiles/topBox/TopBox.tsx";
+import BigChartBox from "./emsFiles/bigChartBox/BigChartBox";
+import ChartBox from "./emsFiles/chartBox/ChartBox";
+import TopBox from "./emsFiles/topBox/TopBox";
+import { chartBoxConversion, chartBoxProduct, chartBoxUser } from "./emsFiles/data";
 import { SideNavDark } from "./sideNavDark";
-import {
-  chartBoxConversion,
-  chartBoxProduct,
-  chartBoxUser,
-} from "./emsFiles/data.ts";
+
 import "./emsFiles/ems.scss";
 import React from 'react';
-export const EMS = (props) => {
+export const EMS = (props: any) => {
   return (
     <div className="home">
       <div className="box1">
