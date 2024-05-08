b0e9698f4c6d367297058024a12cc39005f7feb3
Indraneel Bhattacharjee <indraneelbhattacharjee24@gmail.com>
2024-05-08 15:47:33 -0700

    adding vercel file to root

--- backend/server.js
+++ backend/server.js
@@ -203,7 +203,7 @@ app.use((err, req, res, next) => {
 
 /*app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});*/
+});*
 
 
 // Serverless handler export
